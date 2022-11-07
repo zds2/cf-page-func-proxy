dd1b98d2668249c4071b16a63c1b7c4e2a210b6e
zds2 <71508695+zds2@users.noreply.github.com>
2022-11-07 15:55:10 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="wp.zdbs.cc";
+        url.hostname="zds.zds666.eu.org";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
